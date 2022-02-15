94b102d18b16284824baf4782ddd1a6317ecdd65
Jayaraman, Karthik (Alpharetta) <karthik.jayaraman@Fiserv.com>
2022-02-15 15:13:25 -0500

    Adding initialize script for linux

---
+++ scripts/initialize.sh
@@ -0,0 +1,7 @@
+npm install --save-dev hardhat
+npm install --save-dev @nomiclabs/hardhat-ethers
+npm install --save-dev @nomiclabs/hardhat-etherscan
+npm install @openzeppelin/contracts
+npm install dotenv --save
+npm install --save-dev ethers@^5.0.0
+npm install --save-dev node-fetch@2
