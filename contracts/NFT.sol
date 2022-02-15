4438df21655734071423c3f84597c1f12cfde4d8
Jayaraman, Karthik (Alpharetta) <karthik.jayaraman@Fiserv.com>
2022-02-15 18:41:03 -0500

    Add base URI and json suffix

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.1;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract NFT is ERC721Full {
+contract NFT is ERC721 {
     using Counters for Counters.Counter;
     Counters.Counter private currentTokenId;
     
