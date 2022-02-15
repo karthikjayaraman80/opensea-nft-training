bf170586bd5dcb1b9ee62052d232fd7099a4c59c
Jayaraman, Karthik (Alpharetta) <karthik.jayaraman@Fiserv.com>
2022-02-15 15:07:18 -0500

    first-deploy

---
+++ hardhat.config.js
@@ -0,0 +1,47 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+ require('dotenv').config();
+ require("@nomiclabs/hardhat-ethers");
+ 
+ const { ALCHEMY_KEY, ACCOUNT_PRIVATE_KEY } = process.env;
+ 
+
+//  const { TASK_COMPILE_SOLIDITY_GET_SOLC_BUILD } = require("hardhat/builtin-tasks/task-names");
+//  const path = require("path");
+ 
+//  subtask(TASK_COMPILE_SOLIDITY_GET_SOLC_BUILD, async (args, hre, runSuper) => {
+//    if (args.solcVersion === "0.8.1") {
+//     const compilerPath = path.join(__dirname, "soljson-v0.8.1.js");
+ 
+//      return {
+//        compilerPath,
+//        isSolcJs: true, // if you are using a native compiler, set this to false
+//        version: args.solcVersion,
+//        // this is used as extra information in the build-info files, but other than
+//        // that is not important
+//        longVersion: "0.8.1"
+//      }
+//    }
+ 
+//    // we just use the default subtask if the version is not 0.8.5
+//    return runSuper();
+//  })
+
+
+ module.exports = {
+    solidity: "0.8.1",
+    defaultNetwork: "rinkeby",
+    networks: {
+     hardhat: {},
+     rinkeby: {
+       url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_KEY}`,
+       accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
+     },
+     ethereum: {
+       chainId: 1,
+       url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_KEY}`,
+       accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
+     },
+   },
+ }
---
+++ scripts/initialize.cmd
@@ -0,0 +1,7 @@
+call npm install --save-dev hardhat
+call npm install --save-dev @nomiclabs/hardhat-ethers
+call npm install --save-dev @nomiclabs/hardhat-etherscan
+call npm install @openzeppelin/contracts
+call npm install dotenv --save
+call npm install --save-dev ethers@^5.0.0
+call npm install --save-dev node-fetch@2
